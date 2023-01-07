563c06685e616ab33ad02eb0e438151118000f3c
jscottransom <james.scottran@gmail.com>
2023-01-06 23:47:57 -0800

    implementation of freestanding rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// System entry point. 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
+// Panic Hanlder required for no_std builds
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
