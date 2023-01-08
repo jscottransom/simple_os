64cfdfb2634929a8f2e8ea06c5562a9afed71477
jscottransom <james.scottran@gmail.com>
2023-01-07 16:50:52 -0800

    minimal kernel update

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,27 @@
 
 use core::panic::PanicInfo;
 
-// System entry point. 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
 // Panic Hanlder required for no_std builds
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+static HELLO: &[u8] = b"Hello World!";
+
+// System entry point. 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
 
